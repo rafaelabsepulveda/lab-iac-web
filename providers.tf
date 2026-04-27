fd0376d7d997fb72aacf3c1a48a6ded083033c7b
rafaelabsepulveda <rafaela.b.sepulveda@gmail.com>
2026-04-26 21:13:12 -0400

    providers.tf

--- providers.tf
+++ providers.tf
@@ -1,3 +1,16 @@
-provider "aws" {
-	region = var.aws_region
+terraform {
+required_version = " >= 1.5.0 "
+required_providers {
+aws = {
+source = " hashicorp /aws "
+version = "~ > 5.0"
+}
+archive = {
+source = " hashicorp / archive "
+version = "~ > 2.4"
+}
+}
+}
+provider " aws" {
+region = var . aws_region
 }
